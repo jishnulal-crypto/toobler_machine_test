4ba0ed5e6e00cea321cf369f330be9c012af7341
jishnulal np <outpersonel@gmail.com>
2023-12-14 23:13:17 +0530

    added_refreshindicator

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -5,11 +5,18 @@ import 'package:get/get.dart';
 import 'app/routes/app_pages.dart';
 
 void main() {
-  runApp(
-    GetMaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
       title: "Application",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
-    ),
-  );
+    );
+  }
 }
