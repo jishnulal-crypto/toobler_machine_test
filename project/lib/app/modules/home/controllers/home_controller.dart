d5e6b64310155bc94b315466aa80a9e2c523dd46
jishnulal np <outpersonel@gmail.com>
2023-12-14 21:42:59 +0530

    added employee_details and city list

--- project/lib/app/modules/home/controllers/home_controller.dart
+++ project/lib/app/modules/home/controllers/home_controller.dart
@@ -1,23 +1,58 @@
 import 'package:get/get.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import '../../../models/employee_model.dart';
 
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
+  var employees = <Employee>[].obs;
+  var filteredEmployees = <Employee>[].obs;
+  var cities = <String>['All'].obs;
+  var selectedCity = 'All'.obs;
 
-  final count = 0.obs;
   @override
   void onInit() {
     super.onInit();
+    ever(employees, (_) {
+      print('employees length: ${employees.length}');
+    });
+
+    fetchData();
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  void fetchData() async {
+    try {
+      final response = await http
+          .get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+
+      if (response.statusCode == 200) {
+        final List<dynamic> data = json.decode(response.body);
+        print(response.body);
+
+        employees.assignAll(data
+            .map((e) => Employee(
+                id: e['id'], name: e['name'], city: e['address']['city']))
+            .toList());
+        filteredEmployees.assignAll(employees);
+        print("within length: ${employees.length}");
+        cities.addAll(employees.map((e) => e.city).toList());
+      } else {
+        print("Error: ${response.statusCode}");
+      }
+    } catch (error) {
+      print("Error fetching data: $error");
+    }
   }
 
-  @override
-  void onClose() {
-    super.onClose();
+  void filterByCity(String city) {
+    print("controlle came here right");
+    selectedCity.value = city;
+    if (city == 'All') {
+      filteredEmployees.assignAll(employees);
+    } else {
+      filteredEmployees
+          .assignAll(employees.where((e) => e.city == city).toList());
+    }
   }
-
-  void increment() => count.value++;
 }
--- project/lib/app/modules/home/views/home_view.dart
+++ project/lib/app/modules/home/views/home_view.dart
@@ -3,20 +3,96 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../controllers/home_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class HomeView extends GetView<HomeController> {
-  const HomeView({Key? key}) : super(key: key);
+class HomeView extends StatelessWidget {
+  final HomeController controller = Get.put(HomeController());
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
+        title: Text('Employee Directory'),
       ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+      body: CustomScrollView(
+        slivers: [
+          _buildAppBar(),
+          _buildCityFilter(),
+          _buildEmployeeList(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildAppBar() {
+    return SliverAppBar(
+      expandedHeight: 200.0,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        title: Text('Employee Directory'),
+      ),
+    );
+  }
+
+  Widget _buildCityFilter() {
+    return SliverToBoxAdapter(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          width: double.infinity,
+          height: 100,
+          child: Row(
+            children: [
+              Text('Filter by City: '),
+              Obx(
+                () => ListView.builder(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: controller.cities.length,
+                  itemBuilder: (context, index) {
+                    final city = controller.cities[index];
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: OutlinedButton(
+                        onPressed: () => controller.filterByCity(city),
+                        style: OutlinedButton.styleFrom(
+                          backgroundColor: controller.selectedCity.value == city
+                              ? Colors.blue
+                              : null, // Change the background color when selected
+                        ),
+                        child: Text(city),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildEmployeeList() {
+    print("is here comtroll");
+    return Obx(
+      () => SliverList(
+        delegate: SliverChildBuilderDelegate(
+          (context, index) {
+            print("something");
+            print(controller.filteredEmployees[index].name);
+            final employee = controller.filteredEmployees[index];
+            return ListTile(
+              title: Text(
+                employee.name,
+              ),
+              subtitle: Text(employee.city),
+              // Add your image widget here
+            );
+          },
+          childCount: controller.filteredEmployees.length,
         ),
       ),
     );
